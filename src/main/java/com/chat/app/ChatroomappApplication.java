a0ff2257c3ad6e66cfb6a8f03a21d13202857df3
Ishraque Hussain <ishraqueshaikh822@gmail.com>
2025-12-28 01:33:08 +0530

    remove sout

--- src/main/java/com/chat/app/ChatroomappApplication.java
+++ src/main/java/com/chat/app/ChatroomappApplication.java
@@ -9,7 +9,6 @@ public class ChatroomappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatroomappApplication.class, args);
-		System.out.println("Hello");
 	}
 
 }
